bdf0d0cce9b2790759bd79e19a330a2628d96cb5
Ansh Kumar <115077985+ansh3108@users.noreply.github.com>
2024-06-18 18:46:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function getRandomSize() {
+    return Math.floor(Math.random() * 50) + 10 + 'px'; // Random size between 10px and 60px
+}
+
+function changeTextProperties() {
+    const textElement = document.getElementById('random-text');
+    textElement.style.color = getRandomColor();
+    textElement.style.fontSize = getRandomSize();
+}
